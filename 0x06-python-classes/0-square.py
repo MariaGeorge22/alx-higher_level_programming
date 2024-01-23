846a71bdf04bc8c349f409a8a5130701c79bd7e5
MariaGeorge22 <maria.george2222@gmail.com>
2024-01-23 03:17:29 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,22 +1,8 @@
 #!/usr/bin/python3
-
-__doc__ = '''
-This is the First Task
-'''
+# 0-square.py by Ehoneah Obed
+"""Defines a square """
 
 
 class Square:
-    __doc__ = '''This is a Square Class
-    for Defining Squares
-
-    attributes: None
-    '''
-
-    def __init__(self) -> None:
-        __doc__ = '''This is the init function
-                for init the class
-                '''
-        pass
-
-
-print(Square.__init__.__doc__)
+    """Does nothing"""
+    pass
