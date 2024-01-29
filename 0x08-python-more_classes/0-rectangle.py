21677e08020ed5dea552430f5c2e3ed281bce567
MariaGeorge22 <maria.george2222@gmail.com>
2024-01-29 03:13:01 -0800

    0-rectangle.py

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+1st Task Module
+"""
+
+
+class Rectangle:
+    """
+    Class that defines a rectangle
+    """
+
+    def __init__(self) -> None:
+        """
+        init function
+        """
+        pass
