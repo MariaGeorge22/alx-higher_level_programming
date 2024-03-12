021821b1bf4b4142701961a49389429278084d96
MariaGeorge22”it config --global user.name MariaGeorge22” <maria.george2222@gmail.com>
2024-03-12 13:10:52 -0700

    100-move_to_utf8.sql

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- lists all rows of the table first_table from the database hbtn_0c_0 in your MySQL server
+USE hbtn_0c_0;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
