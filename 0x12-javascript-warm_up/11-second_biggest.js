26948bad68d3943bfbe54acaa6508f7d3329b854
MariaGeorge22 <maria.george2222@gmail.com>
2024-04-09 21:21:33 +0000

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-    console.log(0);
-  } else {
-    const list = process.argv
-      .slice(2)
-      .map(element => parseInt(element));
-    list.splice(
-      list.findIndex(
-        element => element === Math.max(...list)
-      ),
-      1
-    );
-    const secondLargest = Math.max(...list);
-    console.log(secondLargest);
-  }
+  console.log(0);
+} else {
+  const list = process.argv
+    .slice(2)
+    .map(element => parseInt(element));
+  list.splice(
+    list.findIndex(
+      element => element === Math.max(...list)
+    ),
+    1
+  );
+  const secondLargest = Math.max(...list);
+  console.log(secondLargest);
+}
