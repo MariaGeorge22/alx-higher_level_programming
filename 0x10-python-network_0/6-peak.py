03676e83be1734555ff100306ee25e4074f4f179
MariaGeorge22”git config --global user.name MariaGeorge22” <maria.george2222@gmail.com>
2024-04-27 06:49:46 -0700

    6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+a function that finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+        """
+            find peak
+                """
+                    if list_of_integers:
+                                list_of_integers.sort()
+                                        return list_of_integers[-1]
+                                        else:
+                                                    return None
